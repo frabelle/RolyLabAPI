776f4b78a221377d1e4616c66908cc022467fec3
LuisFCE <luisfelipece17@gmail.com>
2021-11-24 22:08:18 -0600

    Initial Commit

---
+++ Aplicacion/Sexos/SexosConsulta.cs
@@ -0,0 +1,32 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Aplicacion.Pais.Consulta;
+
+namespace Aplicacion.Sexos
+{
+    public class SexosConsulta
+    {
+        public class EjecutaSexos : IRequest<List<TblCatSexos>> { }
+
+        public class ManejadorSexos : IRequestHandler<EjecutaSexos, List<TblCatSexos>>
+        {
+            private readonly netLisContext _context;
+            public ManejadorSexos(netLisContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<TblCatSexos>> Handle(EjecutaSexos request, CancellationToken cancellationToken)
+            {
+                var sexos = await _context.TblCatSexos.ToListAsync();
+                return sexos;
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Dominio.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly netLisContext context;
+
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TblMedico> Get()
+        {
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            return context.TblMedicos.ToList();
+        }
+    }
+}
