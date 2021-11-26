a355beded2b28ae530e74827284dda025a026260
LuisFCE <luisfelipece17@gmail.com>
2021-11-26 17:31:02 -0600

    Se añadio el controlador de Perfil

---
+++ Aplicacion/Perfil/PerfilConsulta.cs
@@ -0,0 +1,31 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Perfil
+{
+    public class PerfilConsulta
+    {
+        public class EjecutaPerfil : IRequest<List<TblCatPerfiles>> { }
+
+        public class ManejadorPerfil : IRequestHandler<EjecutaPerfil, List<TblCatPerfiles>>
+        {
+            private readonly netLisContext _context;
+            public ManejadorPerfil(netLisContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<TblCatPerfiles>> Handle(EjecutaPerfil request, CancellationToken cancellationToken)
+            {
+                var perfiles = await _context.TblCatPerfiles.ToListAsync();
+                return perfiles;
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/PerfilController.cs
@@ -0,0 +1,21 @@
+﻿using Dominio.Model;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacion.Perfil;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilController : MiControllerBase
+    {
+        public async Task<ActionResult<List<TblCatPerfiles>>> Get()
+        {
+            return await Mediator.Send(new PerfilConsulta.EjecutaPerfil());
+        }
+    }
+}
